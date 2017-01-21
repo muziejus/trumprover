e1f09aee9e5aeafb5fd6aff5a8d6bf30906e6c68
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-01-21 12:38:13 -0500

    get tweet text to appear.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ ruby '2.3.1'
 
 gem 'sinatra'
 gem 'sinatra-flash'
-gem 'httparty'
+# use this to install nokogiri:
+# gem install nokogiri -- --use-system-libraries --with-xml2-lib=/usr/local/opt/libxml2/lib --with-xml2-include=/usr/local/opt/libxml2/include/libxml2
+gem 'nokogiri'
 gem 'selenium-webdriver'
 gem 'rmagick'
 gem 'dotenv'
--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 require "sinatra/base"
-require "sinatra/flash"
+require "nokogiri"
+require "open-uri"
 require "dotenv"
 require "selenium-webdriver"
 
@@ -11,12 +12,6 @@ class App < Sinatra::Base
 
   use Rack::Session::Cookie,
     secret: ENV['COOKIE']
-  # if Sinatra::Base.development?
-  #   set secret: "supersecret"
-  #   # set :session_secret, "supersecret"
-  # end
-
-  register Sinatra::Flash
 
   get "/" do
     erb :index
@@ -27,16 +22,24 @@ class App < Sinatra::Base
     unless tweet_url =~ /^https:\/\// # maybe confused by the "https://" in the hint.
       tweet_url = "https://" + tweet_url
     end
-    d = Selenium::WebDriver.for :firefox
-    d.navigate.to tweet_url
-    if d.find_element(class: "permalink-tweet") && d.find_element(class: "permalink-tweet").attribute("data-screen-name") =~ /^(potus|realdonaldtrump)$/i 
-      tweet_text = d.find_element(class: "TweetTextSize--26px").text
-      puts tweet_text
+    tweet = Nokogiri::HTML(open(tweet_url)).css('div.permalink-tweet')
+    if tweet.attribute('data-screen-name').text =~ /^(potus|realdonaldtrump)$/i
+      tweet_text = tweet.css('p.TweetTextSize--26px').text
+      erb :results, locals: { tweet_text: tweet_text }, layout: :naked
     else
-      puts "doesn't look like a trump tweet after all."
+      erb :not_trump_tweet, { layout: :naked }
     end
-    d.quit
-    erb :results, {layout: :naked}
+    
+
+    # d = Selenium::WebDriver.for :firefox
+    # d.navigate.to tweet_url
+    # if d.find_element(class: "permalink-tweet") && d.find_element(class: "permalink-tweet").attribute("data-screen-name") =~ /^(potus|realdonaldtrump)$/i 
+    #   tweet_text = d.find_element(class: "TweetTextSize--26px").text
+    #   puts tweet_text
+    # else
+    #   puts "doesn't look like a trump tweet after all."
+    # end
+    # d.quit
   end
 
 end
