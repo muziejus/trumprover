c9dc507747f053d70daad8cf03c2a5b090184f34
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-01-21 10:15:54 -0500

    Get Ajax working.

--- app.rb
+++ app.rb
@@ -4,18 +4,27 @@ require "sinatra/base"
 require "sinatra/flash"
 require "dotenv"
 
+Dotenv.load
+
 class App < Sinatra::Base
 
   use Rack::Session::Cookie,
     secret: ENV['COOKIE']
-  if Sinatra::Base.development?
-    set :session_secret, "supersecret"
-  end
+  # if Sinatra::Base.development?
+  #   set secret: "supersecret"
+  #   # set :session_secret, "supersecret"
+  # end
 
   register Sinatra::Flash
 
   get "/" do
     erb :index
+  end
+
+  post "/" do
+    # puts "puts"
+    # puts params[:inputUrl]
+    erb :results, {layout: :naked}
   end
 
 end
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,11 @@
+function postUrl() {
+  $.post($( "#url" ).attr("action"), $( "#url" ).serialize(), function(data){
+    console.log(data);
+    $( "#textarea" ).empty().append(data);
+  }, "text");
+  return false;
+}
+
 $( "#inputUrl" ).change(function() {
   if(!$( "#inputUrl" ).val().match(/twitter.com.(POTUS|therealdonaldtrump).status./i)) {
     $( "#urlForm" ).removeClass("has-success").addClass("has-feedback").addClass("has-error");
@@ -5,7 +13,8 @@ $( "#inputUrl" ).change(function() {
   } else {
     $( "#urlForm" ).removeClass("has-error").addClass("has-success").addClass("has-feedback");
     $( "#urlFormLabel" ).text("Looks like a Donald Trump tweet!");
+    postUrl();
   }
-
 });
   
+$( "#url" ).submit(postUrl);
